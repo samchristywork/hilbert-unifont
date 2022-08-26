f0369553a0b728438cccb03db10f0f0696d2d09c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-25 20:54:22 -0500

    Added averaging functions.
    This averaged data helps me visualize the curve locality for the README.

--- locality_test/locality.c
+++ locality_test/locality.c
@@ -36,6 +36,11 @@ int main() {
 
   FILE *f1 = fopen("data/line1", "wb");
   FILE *f2 = fopen("data/line2", "wb");
+  FILE *f3 = fopen("data/line1_avg", "wb");
+  FILE *f4 = fopen("data/line2_avg", "wb");
+  float avg_1 = 90;
+  float avg_2 = 90;
+  float factor=.999;
 
   for (int i = 0; i < n * n; i++) {
     // Measure locality using the traditional Gradient Method
@@ -45,7 +50,9 @@ int main() {
       int dx = n / 2 - x;
       int dy = n / 2 - y;
       float dist = sqrt(dx * dx + dy * dy);
+      avg_1=avg_1*factor+dist*(1-factor);
       fprintf(f1, "%f\n", dist);
+      fprintf(f3, "%f\n", avg_1);
     }
 
     // Measure locality using the Hilbert Method
@@ -56,9 +63,13 @@ int main() {
       int dx = n / 2 - x;
       int dy = n / 2 - y;
       float dist = sqrt(dx * dx + dy * dy);
+      avg_2=avg_2*factor+dist*(1-factor);
       fprintf(f2, "%f\n", dist);
+      fprintf(f4, "%f\n", avg_2);
     }
   }
   fclose(f1);
   fclose(f2);
+  fclose(f3);
+  fclose(f4);
 }
